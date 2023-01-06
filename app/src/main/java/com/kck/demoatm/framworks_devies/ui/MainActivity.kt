3e24673b4ef9438a840b61b17e5b836de8cfb6ca
Kingchenkong <wikeyno@gmail.com>
2023-01-06 12:09:23 +0800

    [Add] init project, in an empty activity, application.

---
+++ app/src/main/java/com/kck/demoatm/framworks_devies/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kck.demoatm.framworks_devies.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kck.demoatm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
