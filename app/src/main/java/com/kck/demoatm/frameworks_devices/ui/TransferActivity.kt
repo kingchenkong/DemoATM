692114888b4df66ef6ba7ab3fd568c582a5cbf59
Kingchenkong <wikeyno@gmail.com>
2023-01-09 12:26:59 +0800

    [Modify] TransferActivity view OK.

--- app/src/main/java/com/kck/demoatm/frameworks_devices/ui/TransferActivity.kt
+++ app/src/main/java/com/kck/demoatm/frameworks_devices/ui/TransferActivity.kt
@@ -1,9 +1,9 @@
 package com.kck.demoatm.frameworks_devices.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
 import com.kck.demoatm.R
